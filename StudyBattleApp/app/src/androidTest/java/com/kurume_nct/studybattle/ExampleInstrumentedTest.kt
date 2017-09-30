00a55cf493d21b25ed92df20fc713d4d9be643e3
Kirby <takasu55220@gmail.com>
2017-09-30 17:24:24 +0900

    画像を追加

---
+++ StudyBattleApp/app/src/androidTest/java/com/kurume_nct/studybattle/ExampleInstrumentedTest.kt
@@ -0,0 +1,27 @@
+package com.kurume_nct.studybattle
+
+import android.content.Context
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumentation test, which will execute on an Android device.
+ *
+ * @see [Testing documentation](http://d.android.com/tools/testing)
+ */
+@RunWith(AndroidJUnit4::class)
+ class ExampleInstrumentedTest {
+@Test
+@Throws(Exception::class)
+ fun useAppContext() {
+ // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+
+assertEquals("com.kurume_nct.studybattle", appContext.getPackageName())
+}
+}
