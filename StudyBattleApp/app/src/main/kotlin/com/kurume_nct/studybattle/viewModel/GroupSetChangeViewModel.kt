2fd887758273972c0dd3c6f92b18421f1b6635cf
DESKTOP-34ARI2H\hanah <hanahanahunachi@gmail.com>
2017-10-01 17:52:13 +0900

    confrictしたところ

--- StudyBattleApp/app/src/main/kotlin/com/kurume_nct/studybattle/viewModel/GroupSetChangeViewModel.kt
+++ StudyBattleApp/app/src/main/kotlin/com/kurume_nct/studybattle/viewModel/GroupSetChangeViewModel.kt
@@ -3,6 +3,7 @@ package com.kurume_nct.studybattle.viewModel
 import android.content.Context
 import android.databinding.BaseObservable
 import android.databinding.Bindable
+import android.util.Log
 import android.view.View
 import com.kurume_nct.studybattle.BR
 
