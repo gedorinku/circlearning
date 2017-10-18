96f84c9b97f2a131f773495cfe1e7b9d5c7c97a4
DESKTOP-34ARI2H\hanah <hanahanahunachi@gmail.com>
2017-10-18 22:47:39 +0900

    Item画面。

--- StudyBattleApp/app/src/main/kotlin/com/kurume_nct/studybattle/viewModel/ItemInfoViewModel.kt
+++ StudyBattleApp/app/src/main/kotlin/com/kurume_nct/studybattle/viewModel/ItemInfoViewModel.kt
@@ -6,19 +6,61 @@ import android.database.Observable
 import android.databinding.BaseObservable
 import android.databinding.Bindable
 import com.kurume_nct.studybattle.BR
+import com.kurume_nct.studybattle.client.ServerClient
+import com.kurume_nct.studybattle.model.UnitPersonal
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 
 /**
  * Created by hanah on 9/25/2017.
  */
-class ItemInfoViewModel(private val context: Context, private val callback : Callback) : BaseObservable(){
+class ItemInfoViewModel(private val context: Context, private val callback: Callback) : BaseObservable() {
+
+    lateinit var unitPer: UnitPersonal
 
     @Bindable
     var magicCount = "×0"
-    var bombCount = "×0"
-    var cardCount = "×0"
-    var shieldCount = "×0"
+        set(value) {
+            field = value
+            notifyPropertyChanged(BR.magicCount)
+        }
 
-    interface Callback{
+    @Bindable
+    var bombCount = "×0"
+        set(value) {
+            field = value
+            notifyPropertyChanged(BR.bombCount)
+        }
+
+    @Bindable
+    var cardCount = "×0"
+        set(value) {
+            field = value
+            notifyPropertyChanged(BR.cardCount)
+        }
+
+    @Bindable
+    var shieldCount = "×0"
+        set(value) {
+            field = value
+            notifyPropertyChanged(BR.shieldCount)
+        }
+
+    fun onCreate(){
+        unitPer = context.applicationContext as UnitPersonal
+        ServerClient(unitPer.authenticationKey)
+                .getMyItems(unitPer.nowGroup.id)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe {
+                    magicCount = "×" + it[0].toString()
+                    bombCount = "×" +  it[1].toString()
+                    cardCount = "×" + it[3].toString()
+                    shieldCount = "×" + it[2].toString()
+                }
+    }
+
+    interface Callback {
 
     }
 }
