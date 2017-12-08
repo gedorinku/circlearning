169fc1f34245f867ada4412040e9188ab0792705
DESKTOP-34ARI2H\hanah <hanahanahunachi@gmail.com>
2017-12-08 10:13:02 +0900

    個々のアイテムに対するViewをlistで管理に.

--- StudyBattleApp/app/src/main/kotlin/com/kurume_nct/studybattle/view/LotteryActivity.kt
+++ StudyBattleApp/app/src/main/kotlin/com/kurume_nct/studybattle/view/LotteryActivity.kt
@@ -2,35 +2,28 @@ package com.kurume_nct.studybattle.view
 
 import android.app.Activity
 import android.databinding.DataBindingUtil
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import android.os.Bundle
-import android.os.Handler
 import android.view.View
 import android.widget.ImageView
 
 import com.bumptech.glide.Glide
 import com.kurume_nct.studybattle.R
 import com.kurume_nct.studybattle.databinding.ActivityLotteryBinding
-import com.kurume_nct.studybattle.model.*
 
 class LotteryActivity : Activity() {
 
     private lateinit var binding: ActivityLotteryBinding
     /*当たるアイテム*/
     private var itemNumber = 0
-    /*アイテムの番号*/
-    private val Bomb = 1
-    private val Shield = 2
-    private val DoubleScoreCard = 3
-    private val MagicHand = 4
+    /*アイテムごとのView*/
+    private val itemResource = listOf(R.drawable.gagan, R.drawable.bomb, R.drawable.shield, R.drawable.card, R.drawable.magichand)
+    private val itemSubText = listOf("アイテム獲得ならず.....", "爆弾GET！", "2倍カードGET!", "マジックハンドGET！", "シールドGET!")
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_lottery)
-
         itemNumber = intent.getIntExtra("item", 278)
 
         viewInitSetting()
@@ -41,10 +34,10 @@ class LotteryActivity : Activity() {
     private fun viewInitSetting() {
 
         /*カードをかぶせる*/
-        Glide.with(this).run {
-            load(R.drawable.lotterycard).into(binding.cardLeft)
-            load(R.drawable.lotterycard).into(binding.cardCenter)
-            load(R.drawable.lotterycard).into(binding.cardRight)
+        Glide.with(this).load(R.drawable.lotterycard).run {
+            into(binding.cardLeft)
+            into(binding.cardCenter)
+            into(binding.cardRight)
         }
 
         binding.apply {
@@ -61,52 +54,19 @@ class LotteryActivity : Activity() {
     private fun onOpeningCard(chooseImageView: ImageView) {
 
         binding.apply {
+
             cardLeft.isClickable = false
             cardCenter.isClickable = false
             cardRight.isClickable = false
+
+            lotteryText.text = itemSubText[itemNumber]
+            electedItemImage.setImageResource(itemResource[itemNumber])
+            if (itemNumber > 0) electedItemTextImage.visibility = View.VISIBLE
+
         }
 
-        when (itemNumber) {
-            Bomb -> {
-                binding.apply {
-                    lotteryText.text = "爆弾GET！"
-                    chooseImageView.setImageResource(R.drawable.bomb)
-                    electedItemImage.setImageResource(R.drawable.bomb)
-                }
-            }
-            DoubleScoreCard -> {
-                binding.apply {
-                    lotteryText.text = "2倍カードGET!"
-                    chooseImageView.setImageResource(R.drawable.card)
-                    electedItemImage.setImageResource(R.drawable.card)
-                }
-            }
-            MagicHand -> {
-                binding.apply {
-                    lotteryText.text = "マジックハンドGET！"
-                    chooseImageView.setImageResource(R.drawable.magichand)
-                    electedItemImage.setImageResource(R.drawable.magichand)
-                }
-            }
-            Shield -> {
-                binding.apply {
-                    lotteryText.text = "シールドGET!"
-                    chooseImageView.setImageResource(R.drawable.shield)
-                    electedItemImage.setImageResource(R.drawable.shield)
-                }
-            }
-        /*itemNumber == Air*/
-            else -> {
-                binding.apply {
-                    lotteryText.text = "アイテム獲得ならず....."
-                    chooseImageView.setImageResource(R.drawable.hazure)
-                    electedItemImage.setImageResource(R.drawable.gagan)
-                }
-            }
-        }
-
+        chooseImageView.setImageResource(itemResource[itemNumber])
         chooseImageView.visibility = View.VISIBLE
-        if (itemNumber > 0) binding.electedItemTextImage.visibility = View.VISIBLE
 
     }
 }
